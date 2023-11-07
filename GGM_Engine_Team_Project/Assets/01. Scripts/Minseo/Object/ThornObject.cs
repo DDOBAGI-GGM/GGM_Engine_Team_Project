24250316013e3abcaacd7a71474473dcdfdccc86
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-11-07 23:08:57 +0900

    FallingObject, MovingFloor
    떨어지는 오브젝트, 움직이는 오브젝트

--- GGM_Engine_Team_Project/Assets/01. Scripts/Minseo/Object/ThornObject.cs
+++ GGM_Engine_Team_Project/Assets/01. Scripts/Minseo/Object/ThornObject.cs
@@ -4,12 +4,5 @@ using UnityEngine;
 
 public class ThornObject : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            // 플레이어 hp --하는 코드
-            Debug.Log("플레이어 부닥침ㅋㅋ");
-        }
-    }
+
 }
