47f997eaf1bafd1c3ceeb1419d6c5647676b255f
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-11-07 23:19:28 +0900

    ThornObject

--- GGM_Engine_Team_Project/Assets/01. Scripts/Minseo/Object/ThornObject.cs
+++ GGM_Engine_Team_Project/Assets/01. Scripts/Minseo/Object/ThornObject.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class ThornObject : MonoBehaviour
 {
+    private SpriteRenderer _thornObjectRenderer;
+    private BoxCollider2D _thornCollider;
+    private bool isVisible = true;
+    [SerializeField] private float blinkInterval = 1.0f; 
 
+    void Start()
+    {
+        _thornObjectRenderer = GetComponent <SpriteRenderer>();
+        _thornCollider = GetComponent <BoxCollider2D>();
+
+        StartCoroutine(BlinkObstacle());
+    }
+
+    IEnumerator BlinkObstacle()
+    {
+        while (true)
+        {
+            isVisible = !isVisible;
+            _thornObjectRenderer.enabled = isVisible;
+            _thornCollider.enabled = isVisible;
+            yield return new WaitForSeconds(blinkInterval);
+        }
+    }
 }
