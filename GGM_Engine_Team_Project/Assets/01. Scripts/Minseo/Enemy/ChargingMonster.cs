d1292267890cae11ca8285ea461a734563c81f1d
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-11-10 10:03:28 +0900

    HorizontalAreaMonsters

--- GGM_Engine_Team_Project/Assets/01. Scripts/Minseo/Enemy/ChargingMonster.cs
+++ GGM_Engine_Team_Project/Assets/01. Scripts/Minseo/Enemy/ChargingMonster.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 
 public class ChargingMonster : MonoBehaviour
 {
-    public float detectionRange = 5f;   // 플레이어를 감지할 범위
-    public float chargeSpeed = 10f;     // 돌진 속도
-    public float rotationSpeed = 5f;    // 몬스터의 회전 속도
+    public float direction = 5f;   // 플레이어를 감지할 x축 범위
+    public float chargeSpeed = 10f; // 돌진 속도
 
-    private Transform _player;
+    private Transform player;
     private Rigidbody2D _rigid;
-
-    private bool isCharging = false;
-    private bool hasReachedPlayer = false;
 
     private void Start()
     {
-        _player = GameObject.FindGameObjectWithTag("Player").transform; // 태그로 플레이어 찾기
-        _rigid = GetComponent <Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform; // 태그로 플레이어 찾기
+        _rigid = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -27,57 +23,31 @@ public class ChargingMonster : MonoBehaviour
 
     private void DistanceCheck()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position, _player.position);
+        Vector2 frontVec = new Vector2(_rigid.position.x + direction * 0.1f, _rigid.position.y);
 
-        if (distanceToPlayer <= detectionRange && !isCharging)  // 플레이어가 일정 범위 내에 있고 돌진 중이 아니라면 돌진
+        RaycastHit2D _rightRayHit = Physics2D.Raycast(frontVec, Vector3.right, direction, LayerMask.GetMask("Player"));
+        RaycastHit2D _leftRayHit = Physics2D.Raycast(frontVec, Vector3.left, direction, LayerMask.GetMask("Player"));
+
+        Debug.DrawRay(frontVec, Vector3.right * direction, Color.blue);
+        Debug.DrawRay(frontVec, Vector3.left * direction, Color.red);
+
+        if (_rightRayHit.collider != null || _leftRayHit.collider != null)
         {
             ChargeTowardsPlayer();
         }
     }
 
-    private void OnDrawGizmos()
-    {
-        // 감지 범위를 시각적으로 나타내기
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, detectionRange);
-    }
-
     private void ChargeTowardsPlayer()
     {
-        if (!hasReachedPlayer)
-        {
-            isCharging = true;
+        Vector2 targetPosition = new Vector2(player.position.x, _rigid.position.y);
+        Vector2 moveDirection = (targetPosition - _rigid.position).normalized;
 
-            Vector2 direction = (_player.position - transform.position).normalized; //플레이어를 바라보도록 회전
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            _rigid.rotation = angle;
-
-            float distanceToPlayer = Vector3.Distance(transform.position, _player.position);
-
-            if (distanceToPlayer <= detectionRange)
-            {
-                // 돌진 시작
-                Vector2 velocity = direction * chargeSpeed;
-                _rigid.velocity = velocity;
-            }
-            else
-            {
-                // 플레이어가 detectionRange 밖에 있으면 멈추기
-                _rigid.velocity = Vector2.zero;
-                hasReachedPlayer = true;
-            }
-        }
-        else
-        {
-
-        }
+        _rigid.velocity = new Vector2(moveDirection.x * chargeSpeed, _rigid.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("플레이어 닿기");
-        }
+        Debug.Log("플레이어 충돌");
+        chargeSpeed = 0;
     }
 }
