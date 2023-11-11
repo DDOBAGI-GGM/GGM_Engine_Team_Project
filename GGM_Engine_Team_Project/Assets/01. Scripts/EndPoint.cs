37b62ce8909b65585b279a7bbc7f83bf7243d3ea
은총 <shhshsjsjd4@gmail.com>
2023-11-12 03:40:09 +0900

    도착 지점 씬 전환 연결

---
+++ GGM_Engine_Team_Project/Assets/01. Scripts/EndPoint.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    [SerializeField] SceneChanger sceneChanger;
+
+    private const string sceneName = "GameClearScene";
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            sceneChanger.ChangeScene(sceneName);
+        }
+    }
+}
