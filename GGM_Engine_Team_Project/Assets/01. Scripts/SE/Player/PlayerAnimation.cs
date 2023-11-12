92a667c878aa9d24c5a3f95d3e5100039013b1cc
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2023-11-12 23:37:49 +0900

    플레이어 조작감 수정
    - 플레이어 조작감 수정.
    - 플레이어 점프 애니메이션 오류 감지.
    - 플레이어 HP 스크립트 작성

--- GGM_Engine_Team_Project/Assets/01. Scripts/SE/Player/PlayerAnimation.cs
+++ GGM_Engine_Team_Project/Assets/01. Scripts/SE/Player/PlayerAnimation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour, IPlayerAction
+public class PlayerAnimation : MonoBehaviour//, IPlayerAction
 {
     private Animator animator;
     private PlayerMovement movement;
@@ -10,7 +10,6 @@ public class PlayerAnimation : MonoBehaviour, IPlayerAction
     // 헤쉬들 가져오기
     private readonly int walkHash = Animator.StringToHash("is_walking");
     private readonly int jumpHash = Animator.StringToHash("is_jump");
-    private readonly int jumpingHash = Animator.StringToHash("is_jumping");
     private readonly int winHash = Animator.StringToHash("is_win");
     private readonly int attackHash = Animator.StringToHash("is_attack");
     private readonly int climbHash = Animator.StringToHash("is_climb");
@@ -31,32 +30,9 @@ public class PlayerAnimation : MonoBehaviour, IPlayerAction
         animator.SetBool(walkHash, value);
     }
 
-    public void Jump()
+    public void Jump(bool value)
     {
-        // 플레이어 상태에 따라 다르게
-        if (jump_ok)
-        {
-            jump_ok = false;
-            animator.SetTrigger(jumpHash);
-        }
-    }
-
-    public void JumpStart()     // 점프가 시작되었음.
-    {
-        Debug.Log("공중으로 뛰자");
-        animator.SetBool(jumpingHash, true);
-    }
-
-    public void JumpComplete()          // 플레이어 점프_엔드 애니 마지막에 이벤트로 등록됨.
-    {
-        Debug.Log("점프애니가 끝났음!");
-        jump_ok = true;
-    }
-
-    public void JumpingEnd()
-    {
-        //Debug.Log("점프중 애니 끝.");
-        animator.SetBool(jumpingHash, false);
+        animator.SetBool(jumpHash, value);
     }
 
     public void Attack()
--- GGM_Engine_Team_Project/Assets/01. Scripts/SE/Player/PlayerMovement.cs
+++ GGM_Engine_Team_Project/Assets/01. Scripts/SE/Player/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour, IPlayerAction
     
     private Rigidbody2D body;
     private SpriteRenderer spriteRenderer;
-    private CapsuleCollider2D capsuleCollider;          // 시리얼라이즈필드지워주기
+    [SerializeField] private CapsuleCollider2D capsuleCollider;          // 시리얼라이즈필드지워주기
 
     private bool is_typing;
     public bool Is_typing { get { return is_typing; } set { is_typing = value; } }
@@ -24,7 +24,7 @@ public class PlayerMovement : MonoBehaviour, IPlayerAction
     private bool is_ladder;
 
     private PlayerAnimation anim;
-    private Vector2 rayOrigin, wallOrigin;
+    private Vector2 rayOrigin, wallOrigin, raySize;
 
     private void Start()
     {
@@ -32,77 +32,96 @@ public class PlayerMovement : MonoBehaviour, IPlayerAction
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<PlayerAnimation>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
+        raySize = capsuleCollider.size * gameObject.transform.localScale;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Vector2 pos = new Vector2(rayOrigin.x, rayOrigin.y - raycastDistance);
-        Gizmos.DrawWireCube(pos, capsuleCollider.size);
+        Vector2 x = capsuleCollider.bounds.center;
+        Vector2 pos = new Vector2(x.x, x.y - raycastDistance);
+        Gizmos.DrawWireCube(pos, capsuleCollider.size * gameObject.transform.localScale);
+        Vector2 right = new Vector2(x.x + raycastDistance, x.y + 0.2f);
+        Vector2 left = new Vector2(x.x - raycastDistance, x.y + 0.2f);
+        Gizmos.DrawWireCube(right, capsuleCollider.size * gameObject.transform.localScale);
+        Gizmos.DrawWireCube(left, capsuleCollider.size * gameObject.transform.localScale);
+        Gizmos.color = Color.white;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))        // 스페이스바를 누르고 바닥에 있을 때와 애니가 재생중이 아닐 때에만
+        if (!is_typing)
         {
-            Debug.Log("점프키를 누름");
-            if (is_onGround)
+            // 점프
+            if (Input.GetKeyDown(KeyCode.Space))        // 스페이스바를 누르고 바닥에 있을 때와 애니가 재생중이 아닐 때에만
             {
-                Debug.Log("바닥임");
-                if (anim.jump_ok)
-                {
-                    Debug.Log("애니재생중이 아님");
-                    Jump();
-                }
+                Debug.Log("점프키를 누름");
+                Jump();
             }
+        }
+
+        // 플레이어 idle 애니 재생
+        if (body.velocity.x != 0)
+        {
+            anim.Walk(true);
+        }
+        else
+        {
+            anim.Walk(false);
         }
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate()      // 물리 계산
     {
         if (!is_typing)     // 타이핑 중이 아닐 때에만
         {
-            #region 좌우 움직임 및 점프와 벽에 닿았을 때 velocity.x 조절
+            #region 좌우 움직임 및 점프
             float x = Input.GetAxisRaw("Horizontal");
 
-            rayOrigin = capsuleCollider.bounds.center;
-            RaycastHit2D Hit = Physics2D.BoxCast(rayOrigin, capsuleCollider.size, 0f, Vector2.down, raycastDistance, groundMask);
-            // 콜라이더 중심해서, 콜라이더 사이즈 만큼, 콜라이더는 세로로, 회전은 0, 방향은 아래로. 원점에서 갈정도는 0.2f, 감지할 것은 땅.
+            if(x > 0)
+            {
+                spriteRenderer.flipX = false;        // 왼쪽
+            }
+            else if (x < 0)
+            {
+                spriteRenderer.flipX = true;        // 왼쪽
+            }
 
+            rayOrigin = capsuleCollider.bounds.center;
+            RaycastHit2D Hit = Physics2D.BoxCast(rayOrigin, raySize, 0f, Vector2.down, raycastDistance, groundMask);        // 콜라이더 중심해서, 콜라이더 사이즈 만큼, 콜라이더는 세로로, 회전은 0, 방향은 아래로. 원점에서 갈정도는 0.2f, 감지할 것은 땅.
+             
             if (Hit && body.velocity.y == 0)
             {
-                is_onGround = true;        // 바닥에 충돌이 되었고 떨어지고 있지 않은 상태라면
-                anim.JumpingEnd();
+                is_onGround = true;        // 바닥이면
+                anim.Jump(false);        
             }
             else
             {
                 is_onGround = false;
             }
+            #endregion
 
+            #region 벽 관련 레이퀘스트
             wallOrigin = new Vector2(rayOrigin.x, rayOrigin.y + 0.2f);
-            RaycastHit2D HitRight = Physics2D.BoxCast(wallOrigin, capsuleCollider.size, 0f, Vector2.right, raycastDistance, groundMask);
-            RaycastHit2D HitLeft = Physics2D.BoxCast(wallOrigin, capsuleCollider.size, 0f, Vector2.left, raycastDistance, groundMask);
-            if (HitRight || HitLeft)        // 왼쪽이나 오른쪽 벽에 닿았다면 x 를 0으로 바꿔줘서 벽에 닿으면 떨어지게
+            RaycastHit2D HitRight = Physics2D.BoxCast(wallOrigin, raySize, 0f, Vector2.right, raycastDistance, groundMask);
+            RaycastHit2D HitLeft = Physics2D.BoxCast(wallOrigin, raySize, 0f, Vector2.left, raycastDistance, groundMask);
+            if (HitRight)        // 왼쪽이나 오른쪽 벽에 닿았다면 x 를 0으로 바꿔줘서 벽에 닿으면 떨어지게
             {
-                x = 0;
+                if (x > 0)      // 오른쪽으로 가고 있으면
+                {
+                    x = 0;
+                }
             }
+            if (HitLeft)
+            {
+                if (x < 0)
+                {
+                    x = 0;
+                }
+            }
+            #endregion
 
             body.velocity = new Vector2(x * speed, body.velocity.y);
-            if (x < 0)      // 왼쪽
-            {
-                spriteRenderer.flipX = true;
-            }
-            else if (x > 0)
-            {
-                spriteRenderer.flipX = false;
-            }
-
-            if (x != 0)
-            {
-                anim.Walk(true);
-            }
-            else { anim.Walk(false); }
-            #endregion
 
             #region 사다리 - Y 입력받고 중력 조절해줌.
             if (is_ladder)
@@ -120,17 +139,16 @@ public class PlayerMovement : MonoBehaviour, IPlayerAction
         else
         {
             anim.Walk(false);       // 타이핑 중이니까.        아이들로 변경해줘라
-            body.velocity = Vector2.zero;           // 그리고 그 자리에 멈춰라!
+            body.velocity = Vector2.zero;           // 그리고 그 자리에 멈춰라
         }
     }
     
     public void Jump()
     {
-        //Debug.Log("점프에 따른 움직임 - 바닥에 있다면");
-        if (anim.Jump_ok)
+        if (is_onGround)
         {
-            body.AddForce(new Vector2(body.velocity.x, jump), ForceMode2D.Impulse);
-            anim.Jump();
+            body.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            anim.Jump(true);
         }
     }
 
