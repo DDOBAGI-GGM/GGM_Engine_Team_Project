739ff7bf00888e244b51134a094e4ef919d6f0ab
CAFUCH <tjfdk6201@gmail.com>
2023-11-20 00:54:20 +0900

    버그 수정

--- GGM_Engine_Team_Project/Assets/01. Scripts/Managers/GameManager.cs
+++ GGM_Engine_Team_Project/Assets/01. Scripts/Managers/GameManager.cs
@@ -1,12 +1,20 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public partial class GameManager : SINGLETON<GameManager>
 {
+    [Header("Time")]
     [SerializeField] private float timeSlow;
     
+    [Header("Item")]
+    [SerializeField] private int itemCount = 0;
+
     public void TimeSlow() { Time.timeScale = timeSlow; }
     public void TimeReset() { Time.timeScale = 1; }
     public float TimeNormalize(float time) { return time * timeSlow; }
+
+    public void SetItem() { itemCount++; }
+    public int GetItem() { return itemCount; }
 }
